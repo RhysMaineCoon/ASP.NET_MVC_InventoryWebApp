f02954351c6d7070871cd70fee4d3f7140be1ad1
Rhys <96884384+RhysMaineCoon@users.noreply.github.com>
2023-04-22 10:29:55 -0500

    Add project files.

---
+++ MVC_InventoryWebApp/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_InventoryWebApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
