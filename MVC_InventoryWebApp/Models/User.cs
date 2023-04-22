592a88aff1da834552142501a3470297214a27db
Rhys <96884384+RhysMaineCoon@users.noreply.github.com>
2023-04-22 15:47:37 -0500

    Added models

---
+++ MVC_InventoryWebApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_InventoryWebApp.Models
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
