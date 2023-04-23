f26346007e85d40c90cb32752150729e936f9677
Rhys <96884384+RhysMaineCoon@users.noreply.github.com>
2023-04-23 16:59:01 -0500

    Created view and controller for Items. Also linked view, controller, & model for inventory page.

--- MVC_InventoryWebApp/Models/Item.cs
+++ MVC_InventoryWebApp/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,11 @@ namespace MVC_InventoryWebApp.Models
 {
     public class Item
     {
-        public int Id { get; set; }
-        public String Name { get; set; }
-        public String Description { get; set; }
-        public int Quantity { get; set; }
+        public int ItemId { get; set; }
+        public String ItemName { get; set; }
+        public String ItemDescription { get; set; }
+        public int ItemQuantity { get; set; }
 
+       
     }
 }
