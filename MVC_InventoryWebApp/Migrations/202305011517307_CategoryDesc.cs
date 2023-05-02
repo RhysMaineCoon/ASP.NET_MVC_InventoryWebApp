c78597327076524e579bc9e5b2f638afa0d8b35d
Rhys <96884384+RhysMaineCoon@users.noreply.github.com>
2023-05-02 10:11:53 -0500

    Added Category Description

---
+++ MVC_InventoryWebApp/Migrations/202305011517307_CategoryDesc.cs
@@ -0,0 +1,18 @@
+namespace MVC_InventoryWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CategoryDesc : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Categories", "Description", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Categories", "Description");
+        }
+    }
+}
--- MVC_InventoryWebApp/Models/Category.cs
+++ MVC_InventoryWebApp/Models/Category.cs
@@ -9,6 +9,7 @@ namespace MVC_InventoryWebApp.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public virtual ICollection<Item> Items { get; set; }
     }
 }
